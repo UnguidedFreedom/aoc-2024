243156e77ddab6ab31a5a3d4a64aec5a6f46c3b1
Erin V <vernier.benoit@gmail.com>
2024-12-04 10:06:00 +1000

    Day 02 finally happy with iterators

--- README.md
+++ README.md
@@ -12,10 +12,10 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
 | [Day 1](./src/bin/01.rs) | `335.4µs` | `406.9µs` |
-| [Day 2](./src/bin/02.rs) | `211.1µs` | `554.8µs` |
+| [Day 2](./src/bin/02.rs) | `197.7µs` | `504.0µs` |
 | [Day 3](./src/bin/03.rs) | `391.2µs` | `663.8µs` |
 
-**Total: 2.56ms**
+**Total: 2.50ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/02.rs
+++ src/bin/02.rs
@@ -2,11 +2,11 @@
 
 advent_of_code::solution!(2);
 
-fn is_report_safe<'a, I>(report: &'a I) -> bool
+fn is_report_safe<I>(report: I) -> bool
 where
-    I: Iterator<Item = i32> + Clone + 'a,
+    I: Iterator<Item = i32> + Clone,
 {
-    let diffs = report.clone().map_windows(|&[a, b]| b - a);
+    let diffs = report.map_windows(|&[a, b]| b - a);
 
     diffs.clone().all(|x| (1..=3).contains(&x.abs()))
         && diffs
@@ -14,33 +14,30 @@ where
             .all(|x| x)
 }
 
-fn parse_report(report: &str) -> impl Iterator<Item = i32> + Clone + '_ {
+fn parse_report<'a>(report: &'a str) -> impl Iterator<Item = i32> + Clone + 'a {
     report.split_whitespace().map(|s| s.parse::<i32>().unwrap())
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let answer = input
         .lines()
-        .map(parse_report)
-        .filter(|report| is_report_safe(report))
+        .filter(|&report| is_report_safe(parse_report(report)))
         .count();
     Some(answer as u32)
 }
 
-fn report_can_be_safe<'a, I>(report: &'a I) -> bool
+fn report_can_be_safe<I>(report: I) -> bool
 where
-    I: Iterator<Item = i32> + Clone + 'a,
+    I: Iterator<Item = i32> + Clone,
 {
-    if is_report_safe(report) {
+    if is_report_safe(report.clone()) {
         return true;
     }
 
-    let report = &report.clone().collect::<Vec<i32>>();
-
-    (0..report.len()).any(|i| {
+    let rep = report.collect::<Vec<i32>>();
+    (0..rep.len()).any(|i| {
         is_report_safe(
-            &report
-                .iter()
+            rep.iter()
                 .enumerate()
                 .filter_map(|(j, &val)| (i != j).then_some(val)),
         )
@@ -50,8 +47,7 @@ where
 pub fn part_two(input: &str) -> Option<u32> {
     let answer = input
         .lines()
-        .map(parse_report)
-        .filter(|report| report_can_be_safe(report))
+        .filter(|&report| report_can_be_safe(parse_report(report)))
         .count();
     Some(answer as u32)
 }
