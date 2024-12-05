18652df8ca3bd0fc1bf21afe089286e12aeac81c
Erin V <vernier.benoit@gmail.com>
2024-12-05 14:38:10 +1000

    Day 01 itertools

--- README.md
+++ README.md
@@ -11,10 +11,10 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `335.4µs` | `406.9µs` |
+| [Day 1](./src/bin/01.rs) | `116.4µs` | `182.1µs` |
 | [Day 2](./src/bin/02.rs) | `121.5µs` | `418.0µs` |
 | [Day 3](./src/bin/03.rs) | `391.2µs` | `663.8µs` |
 | [Day 4](./src/bin/04.rs) | `483.0µs` | `149.8µs` |
 
-**Total: 2.97ms**
+**Total: 2.53ms**
 <!--- benchmarking table --->
--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,27 +1,24 @@
+use itertools::Itertools;
 use std::collections::HashMap;
+use std::iter::zip;
 
 advent_of_code::solution!(1);
 
-#[macro_use]
-extern crate text_io;
-
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut first: Vec<u32> = Vec::new();
-    let mut second: Vec<u32> = Vec::new();
-    for line in input.lines() {
-        let (a, b): (u32, u32);
-        scan!(line.bytes() => "{} {}", a, b);
-        first.push(a);
-        second.push(b);
-    }
+    let (mut first, mut second): (Vec<u32>, Vec<u32>) = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .unzip();
+
     first.sort();
     second.sort();
 
-    let result = first
-        .iter()
-        .zip(second.iter())
-        .map(|(&a, &b)| a.abs_diff(b))
-        .sum();
+    let result = zip(first, second).map(|(a, b)| a.abs_diff(b)).sum();
 
     Some(result)
 }
@@ -30,15 +27,18 @@ pub fn part_two(input: &str) -> Option<u32> {
     let mut left: HashMap<u32, u32> = HashMap::new();
     let mut right: HashMap<u32, u32> = HashMap::new();
     for line in input.lines() {
-        let (a, b): (u32, u32);
-        scan!(line.bytes() => "{} {}", a, b);
+        let (a, b) = line
+            .split_whitespace()
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect_tuple()
+            .unwrap();
         *left.entry(a).or_insert(0) += 1;
         *right.entry(b).or_insert(0) += 1;
     }
 
     let result = left
         .iter()
-        .filter_map(|(val, count)| Some(val * count * right.get(val)?))
+        .filter_map(|(val, count)| Some(right.get(val)? * val * count))
         .sum();
 
     Some(result)
