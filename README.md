87983a3bcf53030f3d08943bfabb1d8c4ecfc71c
Erin V <vernier.benoit@gmail.com>
2024-12-10 21:08:47 +1000

    Day 10 initial impl

--- README.md
+++ README.md
@@ -34,6 +34,7 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 7](./src/bin/07.rs) | `488.9µs` | `591.4µs` |
 | [Day 8](./src/bin/08.rs) | `48.1µs` | `178.1µs` |
 | [Day 9](./src/bin/09.rs) | `359.8µs` | `16.7ms` |
+| [Day 10](./src/bin/10.rs) | `1.7ms` | `1.1ms` |
 
-**Total: 68.70ms**
+**Total: 71.50ms**
 <!--- benchmarking table --->
---
+++ src/bin/10.rs
@@ -0,0 +1,129 @@
+use itertools::Itertools;
+use rayon::prelude::*;
+use std::collections::HashSet;
+use std::ops::Range;
+
+advent_of_code::solution!(10);
+
+const DIRS: &[(isize, isize); 4] = &[(1, 0), (0, 1), (-1, 0), (0, -1)];
+
+fn trailheads_score(
+    grid: &Vec<Vec<u8>>,
+    (irange, jrange): (&Range<isize>, &Range<isize>),
+    (i, j): (isize, isize),
+    val: u8,
+) -> HashSet<(isize, isize)> {
+    if !(irange.contains(&i) && jrange.contains(&j)) || grid[i as usize][j as usize] != val {
+        return HashSet::new();
+    }
+
+    if val == 9 {
+        return HashSet::from([(i, j)]);
+    }
+
+    DIRS.iter().fold(HashSet::new(), |mut res, &(di, dj)| {
+        let results = trailheads_score(grid, (irange, jrange), (i + di, j + dj), val + 1);
+        res.extend(results.iter());
+        res
+    })
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut starts: Vec<(isize, isize)> = Vec::new();
+
+    let grid = input
+        .lines()
+        .enumerate()
+        .map(|(i, line)| {
+            line.as_bytes()
+                .iter()
+                .enumerate()
+                .map(|(j, char)| {
+                    let val = char - b'0';
+                    if val == 0 {
+                        starts.push((i as isize, j as isize));
+                    }
+                    val
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let irange = 0..grid.len() as isize;
+    let jrange = 0..grid[0].len() as isize;
+
+    let response: u32 = starts
+        .par_iter()
+        .map(|&c| trailheads_score(&grid, (&irange, &jrange), c, 0).len() as u32)
+        .sum::<u32>();
+
+    Some(response)
+}
+
+fn trailheads_rating(
+    grid: &Vec<Vec<u8>>,
+    (irange, jrange): (&Range<isize>, &Range<isize>),
+    (i, j): (isize, isize),
+    val: u8,
+) -> u32 {
+    if !(irange.contains(&i) && jrange.contains(&j)) || grid[i as usize][j as usize] != val {
+        return 0;
+    }
+
+    if val == 9 {
+        return 1;
+    }
+
+    DIRS.par_iter()
+        .map(|&(di, dj)| trailheads_rating(grid, (irange, jrange), (i + di, j + dj), val + 1))
+        .sum::<u32>()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut starts: Vec<(isize, isize)> = Vec::new();
+
+    let grid = input
+        .lines()
+        .enumerate()
+        .map(|(i, line)| {
+            line.as_bytes()
+                .iter()
+                .enumerate()
+                .map(|(j, char)| {
+                    let val = char - b'0';
+                    if val == 0 {
+                        starts.push((i as isize, j as isize));
+                    }
+                    val
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let irange = 0..grid.len() as isize;
+    let jrange = 0..grid[0].len() as isize;
+
+    let response: u32 = starts
+        .par_iter()
+        .map(|&c| trailheads_rating(&grid, (&irange, &jrange), c, 0))
+        .sum::<u32>();
+
+    Some(response)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(36));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(81));
+    }
+}
