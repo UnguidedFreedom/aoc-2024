69a910f14b1b1435322e2d15d57399121072245e
Erin V <vernier.benoit@gmail.com>
2024-12-13 19:24:33 +1000

    Day 13 iterator

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -3,77 +3,74 @@ use text_io::scan;
 advent_of_code::solution!(13);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let mut response: u64 = 0;
-
-    for chunk in input.split("\n\n") {
+    let response = input.split("\n\n").filter_map(|chunk| {
         let (xa, ya, xb, yb, xt, yt): (i64, i64, i64, i64, i64, i64);
         scan!(chunk.bytes() => "Button A: X+{}, Y+{}\nButton B: X+{}, Y+{}\nPrize: X={}, Y={}", xa, ya, xb, yb, xt, yt);
 
         let div = xb * ya - xa * yb;
         if div == 0 {
-            continue;
+            return None;
         }
 
         let rem = ya * xt - xa * yt;
         if rem % div != 0 {
-            continue;
+            return None;
         }
 
         let b = rem / div;
         if b < 0 {
-            continue;
+            return None;
         }
 
         let rem = xt - xb * b;
         if rem % xa != 0 {
-            continue;
+            return None;
         }
         let a = rem / xa;
         if a < 0 {
-            continue;
+            return None;
         }
 
-        response += (3 * a + b) as u64;
-    }
+        Some((3 * a + b) as u64)
+    }).sum::<u64>();
 
     Some(response)
 }
 
+const OFFSET: i64 = 10000000000000;
+
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut response: u64 = 0;
+    let response = input.split("\n\n").filter_map(|chunk| {
+            let (xa, ya, xb, yb, xt, yt): (i64, i64, i64, i64, i64, i64);
+            scan!(chunk.bytes() => "Button A: X+{}, Y+{}\nButton B: X+{}, Y+{}\nPrize: X={}, Y={}", xa, ya, xb, yb, xt, yt);
+            let (xt, yt) = (xt + OFFSET, yt + OFFSET);
 
-    for chunk in input.split("\n\n") {
-        let (xa, ya, xb, yb, xt, yt): (i64, i64, i64, i64, i64, i64);
-        scan!(chunk.bytes() => "Button A: X+{}, Y+{}\nButton B: X+{}, Y+{}\nPrize: X={}, Y={}", xa, ya, xb, yb, xt, yt);
+            let div = xb * ya - xa * yb;
+            if div == 0 {
+                return None;
+            }
 
-        let (xt, yt) = (xt + 10000000000000, yt + 10000000000000);
+            let rem = ya * xt - xa * yt;
+            if rem % div != 0 {
+                return None;
+            }
 
-        let div = xb * ya - xa * yb;
-        if div == 0 {
-            continue;
-        }
+            let b = rem / div;
+            if b < 0 {
+                return None;
+            }
 
-        let rem = ya * xt - xa * yt;
-        if rem % div != 0 {
-            continue;
-        }
+            let rem = xt - xb * b;
+            if rem % xa != 0 {
+                return None;
+            }
+            let a = rem / xa;
+            if a < 0 {
+                return None;
+            }
 
-        let b = rem / div;
-        if b < 0 {
-            continue;
-        }
-
-        let rem = xt - xb * b;
-        if rem % xa != 0 {
-            continue;
-        }
-        let a = rem / xa;
-        if a < 0 {
-            continue;
-        }
-
-        response += (3 * a + b) as u64;
-    }
+            Some((3 * a + b) as u64)
+        }).sum::<u64>();
 
     Some(response)
 }
